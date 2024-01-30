36618b231f27adfc2a9f05c32999a9c5b664f0e9
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-01-30 10:56:59 -0300

    Alterando font weight do botão primário'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,9 @@ import "./globals.css";
 export default function Home() {
   return (
     <>
-      <Botao
-        fullWidth
-        onClick={(e) => {
-          e.preventDefault();
-          console.log("Clicou primário");
-        }}
-      >
-        Botão
-      </Botao>
-      <Botao
-        fullWidth
-        secundario
-        onClick={(e) => {
-          e.preventDefault();
-          console.log("Clicou secundário");
-        }}
-      >
-        Botão
+      <Botao>
+        <span>Botão</span>
+        
       </Botao>
     </>
   );
