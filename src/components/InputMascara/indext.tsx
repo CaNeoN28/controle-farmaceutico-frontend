b26ee71039b3eb1e9699984faf31b74700cf1d17
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-01-31 10:37:48 -0300

    Customizando input de mascara

--- src/components/InputMascara/indext.tsx
+++ src/components/InputMascara/indext.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import InputMask from "react-input-mask";
+import styles from "./InputMascara.module.scss";
 
 interface Props extends React.ComponentPropsWithoutRef<"input"> {
   mask: string;
 }
 
 export default function InputMascara({ mask, ...props }: Props) {
-  return <InputMask mask={mask} {...props} />;
+  return <InputMask className={styles.input} mask={mask} {...props} />;
 }
