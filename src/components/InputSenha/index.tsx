23d57d97b68d1791b87bc2ec571d6f3da3c67495
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-01-31 09:59:09 -0300

    Animando o ícone de mostrar senha

--- src/components/InputSenha/index.tsx
+++ src/components/InputSenha/index.tsx
@@ -1,22 +1,40 @@
 import React, { useState } from "react";
 import styles from "./InputSenha.module.scss";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import classNames from "classnames";
 
 interface Props extends React.ComponentPropsWithoutRef<"input"> {}
 
 export default function InputSenha({ ...props }: Props) {
   const [ativo, setAtivo] = useState(true);
+  const [contador, setContador] = useState(1);
 
   const alterarAtivo = (e: React.MouseEvent) => {
     e.preventDefault();
+
+    if (contador != 0) setContador(contador - 1);
+
     setAtivo(!ativo);
   };
+
+  const classAtivo = classNames({
+    [styles.icone]: true,
+    [styles.ativo]: ativo,
+    [styles.inativo]: !ativo,
+  });
+
+  const classInativo = classNames({
+    [styles.icone]: true,
+    [styles.ativo]: !ativo,
+    [styles.inativo]: contador == 0 && ativo,
+  });
 
   return (
     <div className={styles.container}>
       <input className={styles.input} type={ativo ? "password" : "text"} />
       <button className={styles.button} onClick={alterarAtivo}>
-        {ativo ? <FaEye/> : <FaEyeSlash/>}
+        <FaEye className={classAtivo} />
+        <FaEyeSlash className={classInativo} />
       </button>
     </div>
   );
