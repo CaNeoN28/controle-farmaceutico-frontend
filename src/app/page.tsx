c14539d5173080a9f5b1a3bae9449e3bd9659e7b
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-01-31 10:42:52 -0300

    Limpando o home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,8 @@
 "use client";
 
-import Input from "@/components/Input";
 import "./globals.css";
-import InputContainer from "@/components/InputContainer";
-import React, { useEffect, useState } from "react";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
-import Botao from "@/components/Botao";
-import InputSenha from "@/components/InputSenha";
-import InputMascara from "@/components/InputMascara/indext";
+import React from "react";
 
 export default function Home() {
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm<{
-    input: string;
-  }>({
-    defaultValues: {
-      input: "",
-    },
-  });
-
-  const onSubmit: SubmitHandler<{ input: string }> = (data) => {
-    console.log(data);
-  };
-
-  return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <InputContainer id="input" label="Label" error={errors.input} fullWidth>
-          <Controller
-            name="input"
-            control={control}
-            rules={{
-              required: { message: "Obrigatório", value: true },
-              minLength: { message: "Mínimo 3", value: 3 },
-            }}
-            render={({ field }) => (
-              <InputMascara mask="aaa.999.999" {...{...field, ref: null}}/>
-            )}
-          />
-        </InputContainer>
-        <Botao>Aperte</Botao>
-      </form>
-    </>
-  );
+  return <></>;
 }
