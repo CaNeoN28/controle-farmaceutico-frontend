801b399b936fed85adfb8103cf4d2257a67a322f
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-01-31 09:22:38 -0300

    Usando ícones

--- src/components/InputSenha/index.tsx
+++ src/components/InputSenha/index.tsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
+import styles from "./InputSenha.module.scss";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 interface Props extends React.ComponentPropsWithoutRef<"input"> {}
 
 export default function InputSenha({ ...props }: Props) {
   const [ativo, setAtivo] = useState(true);
 
+  const alterarAtivo = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setAtivo(!ativo);
+  };
+
   return (
-    <div>
-      <input type={ativo ? "password" : "text"} />
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          setAtivo(!ativo);
-        }}
-      >
-        Clique
+    <div className={styles.container}>
+      <input className={styles.input} type={ativo ? "password" : "text"} />
+      <button className={styles.button} onClick={alterarAtivo}>
+        {ativo ? <FaEye /> : <FaEyeSlash />}
       </button>
     </div>
   );
