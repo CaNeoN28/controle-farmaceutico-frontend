aaa310cd1abeb0c9433e35b3cab5cb7429114e00
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-06 18:11:07 -0300

    Estilo básico para dia de plantão

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import DiaPlantao from "@/components/DiaPlantao";
+
 export default function Teste() {
-	return <div></div>;
+	return <DiaPlantao data="1aaaa"/>;
 }
--- src/components/DiaPlantao/index.tsx
+++ src/components/DiaPlantao/index.tsx
@@ -1,3 +1,20 @@
-export default function DiaPlantao() {
-	return <button></button>;
+import classNames from "classnames";
+import styles from "./DiaPlantao.module.scss";
+import { MouseEventHandler } from "react";
+
+interface Props {
+	data: string;
+	onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function DiaPlantao({ data, onClick }: Props) {
+	const classes = classNames({
+		[styles.diaplantao]: true,
+	});
+
+	return (
+		<button className={classes} onClick={onClick}>
+			{data}
+		</button>
+	);
 }
