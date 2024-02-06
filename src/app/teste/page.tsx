418ec01dd830997766e643345e539f0126739f6b
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-06 17:54:23 -0300

    Limpando teste

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,23 +1,9 @@
 "use client";
 
-import InputPesquisa from "@/components/InputPesquisa";
-import { useState } from "react";
-
 export default function Teste() {
-	const [texto, setTexto] = useState("");
-
 	return (
 		<div>
-			<InputPesquisa
-				onSubmit={(e) => {
-					e.preventDefault();
-					console.log(texto);
-				}}
-				value={texto}
-				onChange={(e) => {
-					setTexto(e.target.value);
-				}}
-			/>
+			
 		</div>
 	);
 }
