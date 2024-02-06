b2b548d6a4ebf5497be7cf3bf4cd61fc0a37818b
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-06 17:43:15 -0300

    Estilizando o componente de input de pesquisa

--- src/components/InputPesquisa/index.tsx
+++ src/components/InputPesquisa/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
+import styles from "./InputPesquisa.module.scss";
 
 interface Props extends React.ComponentPropsWithoutRef<"input"> {}
 
 export default function InputPesquisa({ ...props }: Props) {
 	return (
-		<div>
-			<input type="text" {...props} />
-			<FaSearch />
+		<div className={styles.container}>
+			<input className={styles.input} type="text" {...props} />
+			<button className={styles.icone}>
+				<FaSearch />
+			</button>
 		</div>
 	);
 }
