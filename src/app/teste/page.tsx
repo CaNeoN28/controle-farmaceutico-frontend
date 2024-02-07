db7257b21c8351d7e9ef337275cff5e2e5312189
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-06 22:02:08 -0400

    Adicionando comportamento ao componente

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,10 +1,28 @@
 "use client";
 
 import DiaPlantao from "@/components/DiaPlantao";
+import { useState } from "react";
 
 export default function Teste() {
+  const [dias, setDias] = useState([
+    "2020/10/10",
+    "2020/10/11",
+    "2020/10/12",
+    "2020/10/13",
+    "2020/10/14",
+    "2020/10/15",
+    "2020/10/16",
+    "2020/10/17",
+    "2020/10/18",
+    "2020/10/19",
+  ])
+
 	return <div>
-		<DiaPlantao data="aaaa"/>
-		<DiaPlantao data="2020/10/10"/>
+		{dias.map((dia, index) => 
+      <DiaPlantao key={index} data={dia} onClick={() => {
+        const filtros = dias.filter((d, i) => i != index)
+        setDias(filtros)
+      }}/>
+    )}
 	</div>;
 }
--- src/components/DiaPlantao/index.tsx
+++ src/components/DiaPlantao/index.tsx
@@ -1,33 +1,42 @@
+import { FaTrash } from "react-icons/fa";
 import classNames from "classnames";
 import styles from "./DiaPlantao.module.scss";
 import { MouseEventHandler } from "react";
 
 interface Props {
-	data: string;
-	onClick?: MouseEventHandler<HTMLButtonElement>;
+  data: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export default function DiaPlantao({ data, onClick }: Props) {
-	const classes = classNames({
-		[styles.diaplantao]: true,
-	});
+  const classes = classNames({
+    [styles.diaplantao]: true,
+    [styles.ativo]: !!onClick,
+  });
 
-	const date = new Date(data);
+  const date = new Date(data);
 
-	if (isNaN(Number(date))) {
-		return <></>;
-	}
+  if (isNaN(Number(date))) {
+    return <></>;
+  }
 
-	const { dia, mes, ano } = {
-		dia: date.getDate(),
-		mes: date.getMonth() + 1,
-		ano: date.getFullYear(),
-	};
+  const { dia, mes, ano } = {
+    dia: date.getDate(),
+    mes: date.getMonth() + 1,
+    ano: date.getFullYear(),
+  };
 
-	return (
-		<button className={classes} onClick={onClick}>
-			<span>{dia}/{mes}</span>
-			<span className={styles.ano}>{ano}</span>
-		</button>
-	);
+  return (
+    <button className={classes} onClick={onClick}>
+      <div className={styles.container}>
+        <span>
+          {dia}/{mes}
+        </span>
+        {onClick && <span className={styles.ano}>{ano}</span>}
+      </div>
+      <div className={styles.icone}>
+        <FaTrash />
+      </div>
+    </button>
+  );
 }
