0304f642b71380fb48c2c0abfe4bc99f08e2cf32
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-06 22:02:52 -0400

    Limpando testes

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,28 +1,5 @@
 "use client";
 
-import DiaPlantao from "@/components/DiaPlantao";
-import { useState } from "react";
-
 export default function Teste() {
-  const [dias, setDias] = useState([
-    "2020/10/10",
-    "2020/10/11",
-    "2020/10/12",
-    "2020/10/13",
-    "2020/10/14",
-    "2020/10/15",
-    "2020/10/16",
-    "2020/10/17",
-    "2020/10/18",
-    "2020/10/19",
-  ])
-
-	return <div>
-		{dias.map((dia, index) => 
-      <DiaPlantao key={index} data={dia} onClick={() => {
-        const filtros = dias.filter((d, i) => i != index)
-        setDias(filtros)
-      }}/>
-    )}
-	</div>;
+  return <div></div>;
 }
