77f65ded15ac18b3d022537d9a4fb3bffc1bfe16
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-07 11:11:48 -0300

    Limpando página de testes

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import OpcaoAdministrativa from "@/components/OpcaoAdministrativa";
-
 export default function Teste() {
 	return (
-		<OpcaoAdministrativa url_destino="https://google.com.br" tipo="entidade">
-			A
-		</OpcaoAdministrativa>
+		<div>
+			
+		</div>
 	);
 }
