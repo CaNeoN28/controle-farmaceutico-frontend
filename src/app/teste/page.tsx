21a67b1074a687d5f5fcfd88e23ca6d5856ef4f4
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-08 16:15:52 -0300

    Organizando componente

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -3,10 +3,13 @@
 import CardFarmacia from "@/components/CardFarmacia";
 
 export default function Teste() {
-
 	return (
 		<div>
-			<CardFarmacia/>
+			<CardFarmacia
+				link_farmacia="/teste"
+				informacao="20/10/2024"
+				nome="Farmácia Augusto"
+			/>
 		</div>
 	);
 }
--- src/components/CardFarmacia/index.tsx
+++ src/components/CardFarmacia/index.tsx
@@ -1,7 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+import { MdLocalPharmacy } from "react-icons/md";
 import styles from "./CardFarmacia.module.scss";
 
-interface Props {}
+interface Props {
+	link_farmacia: string;
+	nome: string;
+	informacao: string;
+	imagem_url?: string;
+}
 
-export default function CardFarmacia({}: Props) {
-	return <a></a>;
+export default function CardFarmacia({
+	link_farmacia,
+	informacao,
+	nome,
+	imagem_url,
+}: Props) {
+	return (
+		<Link href={link_farmacia} className={styles.card_farmacia}>
+			{imagem_url ? (
+				<Image src={imagem_url} alt="imagem" />
+			) : (
+				<div className={styles.placeholder}>
+					<MdLocalPharmacy />
+				</div>
+			)}
+			<div className={styles.dados}>
+				<span>{nome}</span>
+				<span>{informacao}</span>
+			</div>
+		</Link>
+	);
 }
