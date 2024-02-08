cacf26eee5c907d66bbdf63781b447d7ca13b483
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-08 15:12:13 -0300

    Criando base para componente de card de farmácia

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,79 +1,12 @@
 "use client";
 
-import Botao from "@/components/Botao";
-import Select, { Opcao } from "@/components/Select";
-import { useEffect, useLayoutEffect, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+import CardFarmacia from "@/components/CardFarmacia";
 
 export default function Teste() {
-	const opcoes: Opcao[] = [
-		{
-			label: "1",
-			valor: "1",
-		},
-		{
-			label: "2",
-			valor: "2",
-		},
-		{
-			label: "3",
-			valor: "3",
-		},
-		{
-			label: "4",
-			valor: "4",
-		},
-		{
-			label: "5",
-			valor: "5",
-		},
-		{
-			label: "6",
-			valor: "6",
-		},
-		{
-			label: "7",
-			valor: "7",
-		},
-	];
-
-	const [filtro, setFiltro] = useState("");
-	const [opcoesFiltradas, setOpcoes] = useState(opcoes);
-
-	const { handleSubmit, control, setValue } = useForm({ defaultValues: { select: 3 } });
-
-	const onSubmit = (data: any) => {
-		console.log(data);
-	};
-
-	useLayoutEffect(() => {
-		const filtrados = opcoes.filter((o) => {
-			const regex = new RegExp(filtro, "i");
-
-			return regex.test(o.label);
-		});
-
-		setOpcoes(filtrados);
-	}, [filtro]);
 
 	return (
 		<div>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<Controller
-					name="select"
-					control={control}
-					render={({ field }) => (
-						<Select
-							{...{ ...field, ref: null }}
-							filtro={filtro}
-							opcoes={opcoesFiltradas}
-							setFiltro={setFiltro}
-							setValue={setValue}
-						/>
-					)}
-				/>
-				<Botao type="submit">Enviar</Botao>
-			</form>
+			<CardFarmacia/>
 		</div>
 	);
 }
---
+++ src/components/CardFarmacia/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./CardFarmacia.module.scss";
+
+interface Props {}
+
+export default function CardFarmacia({}: Props) {
+	return <a></a>;
+}
