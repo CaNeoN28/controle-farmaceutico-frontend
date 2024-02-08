38dc38baa190038cee8302e7daa8223c4ed4b01f
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-08 16:50:40 -0300

    Limpando página de teste

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import CardFarmacia from "@/components/CardFarmacia";
-
 export default function Teste() {
 	return (
 		<div>
-			<CardFarmacia
-				link_farmacia="/teste"
-				informacao="20/10/2024"
-				nome="Farmácia Augusto"
-				imagem_url="https://www.ufmt.br/ocs/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png"
-			/>
+			
 		</div>
 	);
 }
