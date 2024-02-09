3b62f538f415b3060e53324d2d75d074d36a41e1
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-08 20:22:40 -0400

    Limpando página de testes

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -5,26 +5,7 @@ import Secao from "@/components/Secao";
 export default function Teste() {
 	return (
 		<div style={{width:"500px"}}>
-			<Secao titulo="Teste">
-        <ul>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-          <li>Teste</li>
-        </ul>
-      </Secao>
+			
 		</div>
 	);
 }
