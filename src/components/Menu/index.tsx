b965fd153eb4ae38d7852d69b733d43ac92bf131
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-09 10:55:58 -0300

    Pegando o tamanho da tela para o menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import styles from "./Menu.module.scss";
 
 interface Props {}
 
+const getWidth = () => {
+	const { innerWidth } = window;
+
+	return innerWidth;
+};
+
 export default function Menu({}: Props) {
-	return <div></div>;
+	const [width, setWidth] = useState(getWidth());
+
+	useEffect(() => {
+		window.addEventListener("resize", () => {
+			setWidth(getWidth());
+		});
+
+		return () => {
+			window.removeEventListener("resize", () => {
+				setWidth(getWidth());
+			});
+		};
+	}, []);
+
+	if (width >= 480) {
+		return <div>Teste</div>;
+	}
+
+	return <></>;
 }
