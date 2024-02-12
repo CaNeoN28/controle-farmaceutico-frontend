ece5f0602634d43b70dfc803fa11d72279a0f899
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-12 19:37:48 -0300

    Usando variável de ambiente para url da API

--- src/fetch/farmacias.ts
+++ src/fetch/farmacias.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const { API_URL } = process.env;
-const url = `http://localhost:3030/farmacias`;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const url = `${API_URL}/farmacias`;
 
 interface ParamsProximas {
 	pagina?: number;
