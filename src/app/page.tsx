2b3206b74fc801d31eaee62c1a058491106b28b9
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-12 08:41:36 -0300

    Estrutura base para exibição da tela inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
 "use client";
 
-import "./globals.css";
+import Menu from "@/components/Menu";
 import React from "react";
+import "./globals.css";
+import styles from "./Home.module.scss";
+import TituloFarmacia from "@/components/TituloFarmacia";
+import Botao from "@/components/Botao";
+import { redirect } from "next/dist/server/api-utils";
 
 export default function Home() {
-  return <></>;
+	return (
+		<>
+			<Menu />
+			<main className={styles.main}>
+				<div className={styles.farmacia_proxima}>
+					<div className={styles.map}></div>
+					<TituloFarmacia>
+						<span>Farmácia são José</span>
+						<span>Farmácia aberta mais próxima</span>
+					</TituloFarmacia>
+					<Botao fullWidth onClick={() => {}}>Traçar Rota</Botao>
+				</div>
+				<div className={styles.farmacias}>
+					<div className={styles.listagem}></div>
+					<div className={styles.listagem}></div>
+				</div>
+			</main>
+		</>
+	);
 }
--- src/app/teste/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import Menu from "@/components/Menu";
-
-export default function Teste() {
-	return <Menu />;
-}
