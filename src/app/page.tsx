0a26095637d95a1c0f223aa348034df272ebbb3f
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-12 09:11:44 -0300

    Estilização básica

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import styles from "./Home.module.scss";
 import TituloFarmacia from "@/components/TituloFarmacia";
 import Botao from "@/components/Botao";
 import { redirect } from "next/dist/server/api-utils";
+import FarmaciaItem from "@/components/FarmaciaItem";
 
 export default function Home() {
 	return (
@@ -14,16 +15,46 @@ export default function Home() {
 			<Menu />
 			<main className={styles.main}>
 				<div className={styles.farmacia_proxima}>
-					<div className={styles.map}></div>
-					<TituloFarmacia>
-						<span>Farmácia são José</span>
-						<span>Farmácia aberta mais próxima</span>
-					</TituloFarmacia>
-					<Botao fullWidth onClick={() => {}}>Traçar Rota</Botao>
+					<div className={styles.farmacia}>
+						<div className={styles.map}></div>
+						<TituloFarmacia>
+							<div className={styles.info}>
+								<span>Farmácia são José</span>
+								<span>Farmácia aberta mais próxima</span>
+							</div>
+						</TituloFarmacia>
+					</div>
+					<Botao fullWidth onClick={() => {}}>
+						Traçar Rota
+					</Botao>
 				</div>
 				<div className={styles.farmacias}>
-					<div className={styles.listagem}></div>
-					<div className={styles.listagem}></div>
+					<div className={styles.listagem}>
+						<span className={styles.title}>Outras farmácias abertas</span>
+						<div className={styles.items}>
+							{[1, 2, 3, 4, 5, 6].map((v) => (
+								<FarmaciaItem
+									informacao="07:00 - 17:00"
+									nome="Farmácia"
+									para=""
+								/>
+							))}
+						</div>
+						<Botao secundario fullWidth>
+							Ver mais
+						</Botao>
+					</div>
+					<div className={styles.listagem}>
+						<span className={styles.title}>Plantões nos próximos dias</span>
+						<div className={styles.items}>
+							{[1, 2, 3, 4, 5, 6].map((v) => (
+								<FarmaciaItem informacao="24/10/2024" nome="Farmácia" para="" />
+							))}
+						</div>
+						<Botao secundario fullWidth>
+							Ver mais
+						</Botao>
+					</div>
 				</div>
 			</main>
 		</>
