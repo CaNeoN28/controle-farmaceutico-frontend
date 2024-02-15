619a120886dae68140cfcd1741a0f963484601b0
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-15 10:41:42 -0300

    Pegando a farmácia da API

--- src/app/farmacias/[id]/page.tsx
+++ src/app/farmacias/[id]/page.tsx
@@ -1,3 +1,10 @@
+"use client";
+
+import Menu from "@/components/Menu";
+import FarmaciaFetch from "@/fetch/farmacias";
+import Farmacia from "@/types/Farmacia";
+import { useEffect, useState } from "react";
+
 interface Params {
 	id: string;
 }
@@ -5,5 +12,36 @@ interface Params {
 export default function Farmacia({ params }: { params: Params }) {
 	const { id: farmaciaId } = params;
 
-	return <>{farmaciaId}</>;
+	const fFarmacias = new FarmaciaFetch();
+
+	const [farmacia, setFarmacia] = useState<Farmacia>();
+	const [erroFarmacia, setErroFarmacia] = useState("");
+
+	const getFarmacia = () => {
+		fFarmacias
+			.getFarmacia(farmaciaId)
+			.then((res) => {
+				const resposta = res.data as Farmacia;
+
+				setFarmacia(resposta);
+			})
+			.catch((err) => {
+				setErroFarmacia(`Não foi possível encontrar farmácia: ${err.data}`);
+			});
+	};
+
+	useEffect(() => {
+		getFarmacia();
+	}, []);
+
+	return (
+		<>
+			<Menu />
+			{farmacia ? (
+				<main>{farmacia.nome_fantasia}</main>
+			) : (
+				<span>{erroFarmacia}</span>
+			)}
+		</>
+	);
 }
