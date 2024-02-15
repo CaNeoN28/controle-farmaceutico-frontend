5d0e732034a0377fa1e12e73a60e801c0dd5cd0c
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-15 11:23:17 -0300

    Mostrando mapa e horario de servico

--- src/app/farmacias/[id]/page.tsx
+++ src/app/farmacias/[id]/page.tsx
@@ -4,14 +4,22 @@ import Menu from "@/components/Menu";
 import FarmaciaFetch from "@/fetch/farmacias";
 import Farmacia from "@/types/Farmacia";
 import { useEffect, useState } from "react";
-import styles from "./Farmacia.module.scss"
+import styles from "./Farmacia.module.scss";
 import TituloFarmacia from "@/components/TituloFarmacia";
 import Map from "@/components/Map";
 import Botao from "@/components/Botao";
-
+import DiaSemana, { getDayName } from "@/types/DiasSemana";
+import HorarioServico from "@/components/HorarioServico";
+import HorarioServicoView from "@/components/HorarioServicoView";
 
 interface Params {
 	id: string;
+}
+
+interface Horario {
+	dia_semana: string;
+	entrada: string;
+	saida: string;
 }
 
 export default function Farmacia({ params }: { params: Params }) {
@@ -19,7 +27,13 @@ export default function Farmacia({ params }: { params: Params }) {
 
 	const fFarmacias = new FarmaciaFetch();
 
+	const [date, setDate] = useState(new Date());
 	const [farmacia, setFarmacia] = useState<Farmacia>();
+	const [localizacaoFarmacia, setLocalizacaoFarmacia] = useState({
+		lat: 0,
+		lng: 0,
+	});
+	const [horarios, setHorarios] = useState<Horario[]>([]);
 	const [erroFarmacia, setErroFarmacia] = useState("");
 
 	const getFarmacia = () => {
@@ -39,20 +53,51 @@ export default function Farmacia({ params }: { params: Params }) {
 		getFarmacia();
 	}, []);
 
+	useEffect(() => {
+		if (farmacia) {
+			const { x, y } = farmacia.endereco.localizacao;
+
+			setLocalizacaoFarmacia({
+				lat: Number(x),
+				lng: Number(y),
+			});
+
+			const horarios: Horario[] = Object.keys(farmacia.horarios_servico).map(
+				(v: string) => {
+					const dia = v as DiaSemana;
+
+					return {
+						dia_semana: getDayName(dia),
+						entrada: farmacia.horarios_servico[dia].horario_entrada,
+						saida: farmacia.horarios_servico[dia].horario_saida,
+					};
+				}
+			);
+
+			setHorarios(horarios);
+		}
+	}, [farmacia]);
+
 	return (
 		<>
 			<Menu />
 			{farmacia ? (
 				<main className={styles.main}>
 					<div className={styles.farmacia}>
-						<div className={styles.localizacao}>
-							<Map/>
-							<TituloFarmacia/>
+						<div className={styles.informacao_principal}>
+							<div className={styles.map}>
+								<Map map_center={localizacaoFarmacia} />
+							</div>
+							<TituloFarmacia>{farmacia.nome_fantasia}</TituloFarmacia>
 						</div>
 						<Botao fullWidth>Traçar rota</Botao>
 					</div>
 					<div className={styles.informacoes}>
-						<div className={styles.container}></div>
+						<div className={styles.container}>
+							{horarios.map((h) => (
+								<HorarioServicoView key={h.dia_semana} {...h}/>
+							))}
+						</div>
 						<div className={styles.container}></div>
 					</div>
 				</main>
--- src/types/DiasSemana.ts
+++ src/types/DiasSemana.ts
@@ -1,3 +1,26 @@
+type DiaSemana =
+	| "segunda_feira"
+	| "terca_feira"
+	| "quarta_feira"
+	| "quinta_feira"
+	| "sexta_feira"
+	| "sabado"
+	| "domingo";
+
+const getDayName = (dia: DiaSemana) => {
+	const dias = {
+		"domingo": "Domingo",
+		"segunda_feira": "Segunda feira",
+		"terca_feira": "Terca feira",
+		"quarta_feira": "Quarta feira",
+		"quinta_feira": "Quinta feira",
+		"sexta_feira": "Sexta feira",
+		"sabado": "Sábado",
+	}	
+
+	return dias[dia]
+}
+
 const getDayFromNum = (num: number) : DiaSemana => {
 	const days : DiaSemana[] = [
 		"domingo",
@@ -12,15 +35,7 @@ const getDayFromNum = (num: number) : DiaSemana => {
 	return days[num];
 };
 
-type DiaSemana =
-	| "segunda_feira"
-	| "terca_feira"
-	| "quarta_feira"
-	| "quinta_feira"
-	| "sexta_feira"
-	| "sabado"
-	| "domingo";
 
 export default DiaSemana;
 
-export { getDayFromNum };
+export { getDayFromNum, getDayName };
