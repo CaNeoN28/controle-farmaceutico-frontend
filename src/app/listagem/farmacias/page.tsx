19b2ec199517dc6b368dbe418552899a69704c90
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-16 15:56:25 -0300

    Pegando localização

--- src/app/listagem/farmacias/page.tsx
+++ src/app/listagem/farmacias/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
 import Menu from "@/components/Menu";
 import styles from "./Farmacias.module.scss";
+import { useEffect, useState } from "react";
 
 export default function Farmacias() {
+	const [position, setPosition] = useState({ lat: 0, lng: 0 });
+
+	const getPosition = () => {
+		if(navigator.geolocation){
+			navigator.geolocation.watchPosition((position) => {
+				const { latitude, longitude } = position.coords;
+	
+				setPosition({
+					lat: latitude,
+					lng: longitude,
+				});
+			}, (error) => {
+				console.log(error)
+			});
+		} else {
+			console.log("Não foi possível encontrar localização")
+		}
+	};
+
+	useEffect(() => {
+		getPosition();
+	}, []);
+
+	useEffect(() => {
+		console.log(position)
+	}, [position])
+
 	return (
 		<>
 			<Menu />
