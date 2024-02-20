543a776d94888a7844ce70f1b80f9c169ae1c76b
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-19 21:40:46 -0400

    Estilos básicos com base no figma

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,32 @@
 "use client";
 
-import styles from "./Login.module.scss"
+import InputContainer from "@/components/InputContainer";
+import styles from "./Login.module.scss";
 import Menu from "@/components/Menu";
+import Input from "@/components/Input";
+import InputSenha from "@/components/InputSenha";
+import Botao from "@/components/Botao";
 
 export default function Login() {
   return (
     <>
       <Menu />
-      <main>
-        
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <span className={styles.titulo}>LOGIN</span>
+          <form className={styles.form}>
+            <InputContainer id="email" label="Email">
+              <Input id="email" />
+            </InputContainer>
+            <InputContainer id="senha" label="Senha">
+              <InputSenha id="senha" />
+            </InputContainer>
+          </form>
+          <div className={styles.botoes}>
+            <Botao fullWidth secundario>Esqueci a senha</Botao>
+            <Botao fullWidth>Login</Botao>
+          </div>
+        </div>
       </main>
     </>
   );
