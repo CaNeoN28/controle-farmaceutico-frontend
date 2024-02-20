69280024f72690f1d85f131069c367f334e85f77
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-19 21:25:13 -0400

    Criando base para tela de login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import styles from "./Login.module.scss"
+import Menu from "@/components/Menu";
+
+export default function Login() {
+  return (
+    <>
+      <Menu />
+      <main>
+        
+      </main>
+    </>
+  );
+}
