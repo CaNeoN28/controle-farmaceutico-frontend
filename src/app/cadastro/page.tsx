b764d807d9673e21bc8ebd4dfa39631f3c5ac85b
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-20 14:21:49 -0300

    Estabelecendo estrutura para a criação da página de cadastro

---
+++ src/app/cadastro/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Menu from "@/components/Menu";
+import styles from "./AutoCadastro.module.scss";
+
+export default function AutoCadastro() {
+	return (
+		<>
+			<Menu />
+			<main>
+				<div>
+					<form>{/*Formulário*/}</form>
+					<div>{/*Imagem */}</div>
+				</div>
+			</main>
+		</>
+	);
+}
