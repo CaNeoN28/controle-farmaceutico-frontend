1f74bbf133b2c4a8e44f460a723d1bcd9e3b5cd8
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-20 08:06:36 -0300

    Adicionando box shadow

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,28 +6,53 @@ import Menu from "@/components/Menu";
 import Input from "@/components/Input";
 import InputSenha from "@/components/InputSenha";
 import Botao from "@/components/Botao";
+import classNames from "classnames";
+import { useEffect, useState } from "react";
 
 export default function Login() {
-  return (
-    <>
-      <Menu />
-      <main className={styles.main}>
-        <div className={styles.container}>
-          <span className={styles.titulo}>LOGIN</span>
-          <form className={styles.form}>
-            <InputContainer id="email" label="Email">
-              <Input id="email" />
-            </InputContainer>
-            <InputContainer id="senha" label="Senha">
-              <InputSenha id="senha" />
-            </InputContainer>
-          </form>
-          <div className={styles.botoes}>
-            <Botao fullWidth>Login</Botao>
-            <Botao fullWidth secundario>Esqueci a senha</Botao>
-          </div>
-        </div>
-      </main>
-    </>
-  );
+	const [width, setWidth] = useState(window.innerWidth);
+
+	const classesContainer = classNames({
+		[styles.container]: true,
+		["box-shadow"]: width > 480,
+	});
+
+	useEffect(() => {
+		const getWidth = () => {
+			const { innerWidth } = window;
+
+			setWidth(innerWidth);
+		};
+
+		window.addEventListener("resize", getWidth);
+
+		return () => {
+			window.removeEventListener("resize", getWidth);
+		};
+	}, []);
+
+	return (
+		<>
+			<Menu />
+			<main className={styles.main}>
+				<div className={classesContainer}>
+					<span className={styles.titulo}>LOGIN</span>
+					<form className={styles.form}>
+						<InputContainer id="email" label="Email">
+							<Input id="email" />
+						</InputContainer>
+						<InputContainer id="senha" label="Senha">
+							<InputSenha id="senha" />
+						</InputContainer>
+					</form>
+					<div className={styles.botoes}>
+						<Botao fullWidth>Login</Botao>
+						<Botao fullWidth secundario>
+							Esqueci a senha
+						</Botao>
+					</div>
+				</div>
+			</main>
+		</>
+	);
 }
