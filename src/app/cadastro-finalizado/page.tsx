32a7461741febf39efd28b15f347107cfbc1cd30
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-22 15:33:02 -0300

    Estilizando o conteúdo

--- src/app/cadastro-finalizado/page.tsx
+++ src/app/cadastro-finalizado/page.tsx
@@ -2,13 +2,22 @@
 
 import Menu from "@/components/Menu";
 import styles from "./CadastroFinalizado.module.scss";
+import LinkButton from "@/components/LinkButton";
 
 export default function CadastroFinalizado() {
 	return (
 		<>
 			<Menu />
-			<main>
-				
+			<main className={styles.main}>
+				<div className={styles.container}>
+					<div className={styles.text}>
+						<span className={styles.title}>USUÁRIO CADASTRADO</span>
+						<span>
+							Por favor aguarde a verificação de um administrador
+						</span>
+					</div>
+					<LinkButton link="/">Voltar</LinkButton>
+				</div>
 			</main>
 		</>
 	);
