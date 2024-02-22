bea796ec13a84ef0dd069a00c7bcba13da6d1fb8
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-22 16:01:17 -0300

    Estrutura base da página

---
+++ src/app/administracao/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Menu from "@/components/Menu";
+import styles from "./Administracao.module.scss";
+
+export default function Administracao() {
+	return (
+		<>
+			<Menu />
+			<main></main>
+		</>
+	);
+}
