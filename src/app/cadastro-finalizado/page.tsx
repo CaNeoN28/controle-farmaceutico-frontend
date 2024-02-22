13cfe61402e8217a3621eba97a163000b844cddd
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-22 15:53:50 -0300

    Adicionando responsividade

--- src/app/cadastro-finalizado/page.tsx
+++ src/app/cadastro-finalizado/page.tsx
@@ -8,13 +8,11 @@ export default function CadastroFinalizado() {
 	return (
 		<>
 			<Menu />
-			<main className={styles.main}>
-				<div className={styles.container}>
+			<main className={styles.container}>
+				<div className={styles.content}>
 					<div className={styles.text}>
 						<span className={styles.title}>USUÁRIO CADASTRADO</span>
-						<span>
-							Por favor aguarde a verificação de um administrador
-						</span>
+						<span>Por favor aguarde a verificação de um administrador</span>
 					</div>
 					<LinkButton link="/">Voltar</LinkButton>
 				</div>
