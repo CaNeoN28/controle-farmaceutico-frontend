dcc8d8e151938d60d812bc8d088bcae224ccb9a6
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-22 15:25:09 -0300

    Estruturando base para a página

---
+++ src/app/cadastro-finalizado/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Menu from "@/components/Menu";
+import styles from "./CadastroFinalizado.module.scss";
+
+export default function CadastroFinalizado() {
+	return (
+		<>
+			<Menu />
+			<main>
+				
+			</main>
+		</>
+	);
+}
