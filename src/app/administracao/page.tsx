bbb2fd3214c744a20abe5b7e00dc7c824095d00b
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-22 16:10:57 -0300

    Estilização base dos componentes

--- src/app/administracao/page.tsx
+++ src/app/administracao/page.tsx
@@ -2,12 +2,46 @@
 
 import Menu from "@/components/Menu";
 import styles from "./Administracao.module.scss";
+import OpcaoAdministrativa from "@/components/OpcaoAdministrativa";
 
 export default function Administracao() {
 	return (
 		<>
 			<Menu />
-			<main></main>
+			<main className={styles.main}>
+				<div className={styles.opcoes}>
+					<div className={styles.conjunto}>
+						<OpcaoAdministrativa tipo="farmacia" url_destino="farmacias">
+							Listagem de Farmácias
+						</OpcaoAdministrativa>
+						<OpcaoAdministrativa
+							tipo="farmacia"
+							url_destino="farmacias/cadastro"
+						>
+							Cadastro de Farmácia
+						</OpcaoAdministrativa>
+					</div>
+					<div className={styles.conjunto}>
+						<OpcaoAdministrativa tipo="usuario" url_destino="usuarios">
+							Listagem de Usuários
+						</OpcaoAdministrativa>
+						<OpcaoAdministrativa tipo="usuario" url_destino="usuarios/cadastro">
+							Cadastro de Usuários
+						</OpcaoAdministrativa>
+					</div>
+					<div className={styles.conjunto}>
+						<OpcaoAdministrativa
+							tipo="entidade"
+							url_destino="entidades/cadastro"
+						>
+							Listagem de Entidades
+						</OpcaoAdministrativa>
+						<OpcaoAdministrativa tipo="entidade" url_destino="entidades">
+							Cadastro de Entidades
+						</OpcaoAdministrativa>
+					</div>
+				</div>
+			</main>
 		</>
 	);
 }
