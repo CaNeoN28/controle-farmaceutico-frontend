9822a861519afde66248a7cd621a5a970e01228c
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-23 09:37:10 -0300

    Organizando estruturas

--- src/app/administracao/farmacias/cadastro/page.tsx
+++ src/app/administracao/farmacias/cadastro/page.tsx
@@ -1,19 +1,61 @@
 "use client";
 
+import Botao from "@/components/Botao";
 import styles from "./CadastroFarmacia.module.scss";
 import Menu from "@/components/Menu";
 import TituloSecao from "@/components/TituloSecao";
 import redirecionarAutenticacao from "@/utils/redirecionarAutenticacao";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import Farmacia from "@/types/Farmacia";
+import InputContainer from "@/components/InputContainer";
+import Input from "@/components/Input";
+import InputMascara from "@/components/InputMascara/indext";
 
 export default function CadastroFarmacia() {
+	const { handleSubmit, control } = useForm<Farmacia>();
+
 	redirecionarAutenticacao();
+
+	const onSubmit: SubmitHandler<Farmacia> = (data) => {
+		console.log(data);
+	};
 
 	return (
 		<>
 			<Menu />
 			<main className={styles.main}>
 				<TituloSecao>CADASTRO DE FARMÁCIA</TituloSecao>
-				<form className={styles.form}></form>
+				<form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+					<div className={styles.secao}>
+						<span className={styles.titulo_secao}>Dados da farmácia</span>
+						<div className={styles.inputs_secao}>
+							<Controller
+								name="cnpj"
+								control={control}
+								render={({ field }) => {
+									return (
+										<InputContainer id="cnpj" label="CNPJ">
+											<InputMascara
+												id="cnpj"
+												mask="99.999.999/9999-99"
+												placeholder="00.000.000/0000.00"
+												{...{ ...field, ref: null }}
+											/>
+										</InputContainer>
+									);
+								}}
+							/>
+						</div>
+					</div>
+					<div className={styles.botoes}>
+						<Botao fullWidth type="submit">
+							Salvar
+						</Botao>
+						<Botao fullWidth secundario>
+							Cancelar
+						</Botao>
+					</div>
+				</form>
 			</main>
 		</>
 	);
