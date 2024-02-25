26df50806d5d44f5d178026ab55c58c73b5f41e9
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-25 15:17:05 -0300

    Enviando dados para a API

--- src/app/administracao/farmacias/cadastro/page.tsx
+++ src/app/administracao/farmacias/cadastro/page.tsx
@@ -1,12 +1,23 @@
 "use client";
 
 import IFarmacia from "@/types/Farmacia";
-import { useState } from "react";
 import FormularioFarmcaia from "../formulario";
+import FetchFarmacia from "@/fetch/farmacias";
+import { getCookie } from "cookies-next";
 
 export default function CadastroFarmacia() {
+	const postFarmacia = new FetchFarmacia().postFarmacia;
+
 	const salvarFarmacia = async (farmacia: IFarmacia) => {
-		console.log(farmacia);
+		const token = getCookie("authentication")
+
+		await postFarmacia(farmacia, token)
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	};
 
 	return <FormularioFarmcaia salvarFarmacia={salvarFarmacia} />;
