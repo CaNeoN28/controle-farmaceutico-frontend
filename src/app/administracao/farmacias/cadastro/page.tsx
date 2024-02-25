16f9ed28dd7dc8d54deea5a28d314e18c0229ed0
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-25 15:47:59 -0300

    Exibindo mensagens de erro

--- src/app/administracao/farmacias/cadastro/page.tsx
+++ src/app/administracao/farmacias/cadastro/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import styles from "./CadastroFarmacia.module.scss";
 import IFarmacia from "@/types/Farmacia";
 import FormularioFarmacia from "../formulario";
 import FetchFarmacia from "@/fetch/farmacias";
 import { getCookie } from "cookies-next";
 import Alert from "@/components/Alert";
 import { useState } from "react";
+import { RequestErro } from "@/types/Requests";
+import Botao from "@/components/Botao";
 
 export default function CadastroFarmacia() {
-	const [showAlert, setShowAlert] = useState(true);
+	const [showAlert, setShowAlert] = useState(false);
+	const [erro, setErro] = useState<string>();
+	const [mensagem, setMensagem] = useState<string>();
 
 	const postFarmacia = new FetchFarmacia().postFarmacia;
 
@@ -19,8 +24,19 @@ export default function CadastroFarmacia() {
 			.then((res) => {
 				console.log(res);
 			})
-			.catch((err) => {
-				console.log(err);
+			.catch((err: RequestErro<any>) => {
+				const {
+					response: { data },
+				} = err as RequestErro<string>;
+
+				if (typeof data === "string") {
+					setErro(data);
+				} else {
+					setErro("Não foi possível cadastrar farmácia");
+					console.error(err.response);
+				}
+
+				setShowAlert(true);
 			});
 	};
 
@@ -28,7 +44,23 @@ export default function CadastroFarmacia() {
 		<>
 			<FormularioFarmacia salvarFarmacia={salvarFarmacia} />
 			<Alert show={showAlert} setShow={setShowAlert}>
-				Teste
+				<div className={styles.alert}>
+					<span className={styles.alert_texto}>{erro || mensagem}</span>
+					<div className={styles.alert_opcoes}>
+						{erro ? (
+							<>
+								<Botao fullWidth>Confirmar</Botao>
+								<Botao secundario fullWidth>
+									Cancelar
+								</Botao>
+							</>
+						) : (
+							<>
+								<Botao fullWidth>Confirmar</Botao>
+							</>
+						)}
+					</div>
+				</div>
 			</Alert>
 		</>
 	);
