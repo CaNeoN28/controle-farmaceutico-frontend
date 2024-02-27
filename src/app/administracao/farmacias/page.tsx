713cc161ab8ecb30f3f6a53a50603a2db8a11401
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-27 10:16:08 -0300

    Usando novo método de paginação

--- src/app/administracao/farmacias/page.tsx
+++ src/app/administracao/farmacias/page.tsx
@@ -3,17 +3,57 @@
 import Menu from "@/components/Menu";
 import styles from "./FarmaciasAdministracao.module.scss";
 import redirecionarAutenticacao from "@/utils/redirecionarAutenticacao";
+import TituloSecao from "@/components/TituloSecao";
+import Paginacao from "@/components/Paginacao";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useLayoutEffect, useState } from "react";
 
-interface Params {
-	searchParams: {};
-}
+export default function FarmaciasAdministracao() {
+	redirecionarAutenticacao();
 
-export default function FarmaciasAdministracao({ searchParams }: Params) {
-	redirecionarAutenticacao()
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const router = useRouter();
+
+	const { pagina} = {
+		pagina: Number(searchParams.get("pagina")) || 1,
+	};
+
+	const [params, setParams] = useState<URLSearchParams>()
+
+	function addSearchParam(chave: string, valor?: string) {
+		const params = new URLSearchParams(searchParams);
+
+		if (valor) {
+			params.set(chave, valor);
+		} else {
+			params.delete(chave);
+		}
+
+		setParams(params)
+	}
+
+	useLayoutEffect(() => {
+		if(params){
+			router.replace(`${pathname}?${params}`)
+		}
+	}, [params])
 
 	return (
 		<>
 			<Menu />
+			<main>
+				<TituloSecao>LISTAGEM DE FARMÁCIAS</TituloSecao>
+				<div></div>
+				<div></div>
+				<Paginacao
+					pagina={pagina}
+					paginaMax={5}
+					setPagina={(v) => {
+						addSearchParam("pagina", v.toString());
+					}}
+				/>
+			</main>
 		</>
 	);
 }
