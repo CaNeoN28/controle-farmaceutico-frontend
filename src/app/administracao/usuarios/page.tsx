b692b65de592763accaf6a850a594e56f8975e63
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-28 08:35:18 -0300

    Pegando usuários da API

--- src/app/administracao/usuarios/page.tsx
+++ src/app/administracao/usuarios/page.tsx
@@ -8,11 +8,64 @@ import {
 	AdministracaoMain,
 } from "@/components/Administracao";
 import TituloSecao from "@/components/TituloSecao";
+import { useEffect, useState } from "react";
+import { IUsuarioAPI } from "@/types/Usuario";
+import { getUsuarios } from "@/fetch/usuarios";
+import { GetManyRequest } from "@/types/Requests";
+import { getCookie } from "cookies-next";
+import FetchAutenticacao from "@/fetch/autenticacao";
 
 interface Filtros {}
 
 export default function UsuariosAdministracao() {
 	redirecionarAutenticacao();
+
+	const fAuth = new FetchAutenticacao();
+
+	const [usuario, setUsuario] = useState<IUsuarioAPI>();
+	const [token, setToken] = useState("");
+
+	const [usuarios, setUsuarios] = useState<IUsuarioAPI[]>([]);
+
+	const getUsuario = async () => {
+		const token = getCookie("authentication") || "";
+
+		await fAuth
+			.getPerfil(token)
+			.then((res) => {
+				const usuario = res.data as IUsuarioAPI;
+
+				setUsuario(usuario);
+				setToken(token);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	const fetchUsuarios = async () => {
+		await getUsuarios({ limite: 10, pagina: 1 }, token)
+			.then((res) => {
+				const { dados } = res.data as GetManyRequest<IUsuarioAPI[]>;
+
+				setUsuarios(dados);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	useEffect(() => {
+		getUsuario();
+	}, []);
+
+	useEffect(() => {
+		if (token) fetchUsuarios();
+	}, [token]);
+
+	useEffect(() => {
+		console.log(usuarios);
+	}, [usuarios]);
 
 	return (
 		<>
