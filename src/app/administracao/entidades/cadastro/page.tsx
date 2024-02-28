424b9a353bcba2be2d7b0e4a34be1d6a849413de
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-28 17:57:03 -0300

    Iniciando formulário

--- src/app/administracao/entidades/cadastro/page.tsx
+++ src/app/administracao/entidades/cadastro/page.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import styles from "./EntidadesCadastro.module.scss";
 import { CadastroMain } from "@/components/Cadastro";
 import Menu from "@/components/Menu";
+import TituloSecao from "@/components/TituloSecao";
+import FormularioEntidade from "../formulario";
 
 export default function CadastroEntidades() {
 	return (
 		<>
 			<Menu />
-			<CadastroMain></CadastroMain>
+			<CadastroMain>
+				<TituloSecao>CADASTRO DE ENTIDADE</TituloSecao>
+				<FormularioEntidade />
+			</CadastroMain>
 		</>
 	);
 }
--- src/app/administracao/entidades/formulario/index.tsx
+++ src/app/administracao/entidades/formulario/index.tsx
@@ -1,8 +1,71 @@
-import { CadastroContainer } from "@/components/Cadastro";
+import {
+	CadastroBotoes,
+	CadastroContainer,
+	CadastroEtapa,
+	CadastroForm,
+	CadastroInputs,
+} from "@/components/Cadastro";
 import styles from "./FormularioEntidade.module.scss";
+import Botao from "@/components/Botao";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import IEntidade from "@/types/Entidades";
+import InputContainer from "@/components/InputContainer";
+import Input from "@/components/Input";
 
-interface Props {}
+interface Props {
+	entidade?: IEntidade;
+}
 
-export default function FormularioEntidade({}: Props) {
-	return <CadastroContainer></CadastroContainer>;
+export default function FormularioEntidade({ entidade }: Props) {
+	const {
+		control,
+		formState: { errors },
+		handleSubmit,
+		setValue,
+	} = useForm<IEntidade>({
+		defaultValues: entidade || {
+			ativo: true,
+			estado: "",
+			municipio: "",
+			nome_entidade: "",
+		},
+	});
+
+	const onSubmit: SubmitHandler<IEntidade> = (data) => {
+		console.log(data);
+	};
+
+	return (
+		<CadastroContainer>
+			<CadastroForm onSubmit={handleSubmit(onSubmit)}>
+				<CadastroEtapa titulo="Dados da entidade">
+					<CadastroInputs>
+						<Controller
+							control={control}
+							name="nome_entidade"
+							render={({ field }) => {
+								return (
+									<InputContainer
+										id="nome_entidade"
+										label="Nome da entidade"
+										error={errors.nome_entidade}
+									>
+										<Input id="nome_entidade" {...{ ...field }} />
+									</InputContainer>
+								);
+							}}
+						/>
+					</CadastroInputs>
+				</CadastroEtapa>
+				<CadastroBotoes>
+					<Botao fullWidth type="submit">
+						Salvar
+					</Botao>
+					<Botao fullWidth secundario>
+						Cancelar
+					</Botao>
+				</CadastroBotoes>
+			</CadastroForm>
+		</CadastroContainer>
+	);
 }
