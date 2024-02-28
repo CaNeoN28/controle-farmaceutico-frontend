cb2ec2ba9d73262cc611796b1f35bbe5bb18b4a1
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-28 10:15:52 -0300

    Definindo campos para os filtros de usuário

--- src/app/administracao/usuarios/page.tsx
+++ src/app/administracao/usuarios/page.tsx
@@ -4,7 +4,9 @@ import redirecionarAutenticacao from "@/utils/redirecionarAutenticacao";
 import styles from "./UsuariosAdministracao.module.scss";
 import Menu from "@/components/Menu";
 import {
+	AdministracaoConfirmarFiltros,
 	AdministracaoContainer,
+	AdministracaoFiltros,
 	AdministracaoItem,
 	AdministracaoListagem,
 	AdministracaoMain,
@@ -21,13 +23,19 @@ import verificarPermissao from "@/utils/verificarPermissao";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { addSearchParam } from "@/utils/navigation";
 import Paginacao from "@/components/Paginacao";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import InputContainer from "@/components/InputContainer";
+import Input from "@/components/Input";
+import Select, { Opcao } from "@/components/Select";
+import FetchEntidades from "@/fetch/entidades";
+import IEntidade from "@/types/Entidades";
 
 interface Filtros {
 	pagina?: number;
 	limite?: number;
 	nome_completo?: string;
-	entidade?: string;
-	funcao?: Funcao;
+	entidade_relacionada?: string;
+	funcao?: Funcao | "";
 }
 
 export default function UsuariosAdministracao() {
@@ -38,18 +46,74 @@ export default function UsuariosAdministracao() {
 	const router = useRouter();
 
 	const fAuth = new FetchAutenticacao();
+	const fEntidades = new FetchEntidades();
 
 	const { pagina }: Filtros = {
 		pagina: Number(searchParams.get("pagina")) || 1,
 	};
+
+	const { control, handleSubmit, watch, setValue } = useForm<Filtros>({
+		defaultValues: {
+			entidade_relacionada: "",
+			funcao: "",
+			nome_completo: "",
+		},
+	});
 
 	const [usuario, setUsuario] = useState<IUsuarioAPI>();
 	const [token, setToken] = useState("");
 
 	const [usuarios, setUsuarios] = useState<IUsuarioAPI[]>([]);
 
+	const [funcoes, setFuncoes] = useState<Opcao[]>([]);
+	const [filtroFuncao, setFiltroFuncao] = useState("");
+
+	const [entidades, setEntidades] = useState<Opcao[]>([]);
+	const [filtroEntidade, setFiltroEntidade] = useState("");
+
 	const [params, setParams] = useState<URLSearchParams>(searchParams);
 	const [maxPaginas, setMaxPaginas] = useState<number>(5);
+
+	const getFuncoes = () => {
+		const funcoes: Funcao[] = [
+			"ADMINISTRADOR",
+			"GERENTE",
+			"USUARIO",
+			"INATIVO",
+		];
+
+		const fFuncoes: Opcao[] = funcoes
+			.filter((f) => new RegExp(filtroFuncao, "i").test(f))
+			.map((f) => {
+				return {
+					label: f,
+					valor: f,
+				};
+			});
+
+		setFuncoes(fFuncoes);
+	};
+
+	const getEntidades = async () => {
+		await fEntidades
+			.getEntidades({ nome_entidade: filtroEntidade })
+			.then((res) => {
+				const { dados } = res.data as GetManyRequest<IEntidade[]>;
+
+				const opcoes: Opcao[] = dados.map((d) => {
+					return {
+						label: d.nome_entidade,
+						valor: d._id,
+					};
+				});
+
+				setEntidades(opcoes)
+			}).catch();
+	};
+
+	const onFiltro: SubmitHandler<Filtros> = (data) => {
+		console.log(data);
+	};
 
 	const getUsuario = async () => {
 		const token = getCookie("authentication") || "";
@@ -100,6 +164,14 @@ export default function UsuariosAdministracao() {
 	}, []);
 
 	useEffect(() => {
+		getFuncoes();
+	}, [filtroFuncao]);
+
+	useEffect(() => {
+		getEntidades();
+	}, [filtroEntidade]);
+
+	useEffect(() => {
 		fetchUsuarios();
 	}, [pagina]);
 
@@ -124,6 +196,65 @@ export default function UsuariosAdministracao() {
 				<AdministracaoMain>
 					<TituloSecao>LISTAGEM DE USUÁRIOS</TituloSecao>
 					<AdministracaoContainer>
+						<AdministracaoFiltros onSubmit={handleSubmit(onFiltro)}>
+							<Controller
+								name="nome_completo"
+								control={control}
+								render={({ field }) => {
+									return (
+										<InputContainer id="nome_completo" label="Nome completo">
+											<Input
+												id="nome_completo"
+												placeholder="Nome completo do usuário"
+												{...{ ...field, ref: null }}
+											/>
+										</InputContainer>
+									);
+								}}
+							/>
+							<Controller
+								control={control}
+								name="funcao"
+								render={({ field }) => {
+									return (
+										<InputContainer id="funcao" label="Função administrativa">
+											<Select
+												id="funcao"
+												placeholder="USUARIO"
+												opcoes={funcoes}
+												filtro={filtroFuncao}
+												setFiltro={setFiltroFuncao}
+												setValue={setValue}
+												{...{ ...field, ref: null }}
+											/>
+										</InputContainer>
+									);
+								}}
+							/>
+							<Controller
+								control={control}
+								name="entidade_relacionada"
+								render={({ field }) => {
+									return (
+										<InputContainer
+											id="entidade_relacionada"
+											label="Entidade relacionada"
+										>
+											<Select
+												id="entidade_relacionada"
+												placeholder="Ministério da Saúde"
+												opcoes={entidades}
+												filtro={filtroEntidade}
+												setFiltro={setFiltroEntidade}
+												setValue={setValue}
+												{...{...field, ref: null}}
+											/>
+										</InputContainer>
+									);
+								}}
+							/>
+							<AdministracaoConfirmarFiltros onClean={() => {}} />
+						</AdministracaoFiltros>
 						{usuarios.length > 0 && (
 							<AdministracaoListagem>
 								{usuarios.map((f, i) => {
