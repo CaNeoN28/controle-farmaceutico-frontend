5157052c531d426b88b9c86de014cd57dd8fee8e
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-02-29 10:19:28 -0300

    Redirecionando se o usuário não estiver logado

--- src/app/administracao/entidades/cadastro/page.tsx
+++ src/app/administracao/entidades/cadastro/page.tsx
@@ -6,19 +6,56 @@ import Menu from "@/components/Menu";
 import TituloSecao from "@/components/TituloSecao";
 import FormularioEntidade from "../formulario";
 import IEntidade from "@/types/Entidades";
+import { useEffect, useState } from "react";
+import { IUsuarioAPI } from "@/types/Usuario";
+import FetchAutenticacao from "@/fetch/autenticacao";
+import { deleteCookie, getCookie } from "cookies-next";
+import redirecionarAutenticacao from "@/utils/redirecionarAutenticacao";
+import { useRouter } from "next/navigation";
 
 export default function CadastroEntidades() {
+	const router = useRouter()
+
+	const fAuth = new FetchAutenticacao();
+
+	const [usuario, setUsuario] = useState<IUsuarioAPI>();
+	const [token, setToken] = useState<string>();
+
+	const getToken = async () => {
+		const token = getCookie("authentication");
+
+		await fAuth
+			.getPerfil(token)
+			.then((res) => {
+				const usuario = res.data as IUsuarioAPI;
+
+				setToken(token);
+				setUsuario(usuario);
+			})
+			.catch(() => {
+				deleteCookie("authentication")
+				router.push("/login")
+			});
+	};
+
 	function enviarEntidade(data: IEntidade) {
 		console.log(data);
 	}
 
-	return (
-		<>
-			<Menu />
-			<CadastroMain>
-				<TituloSecao>CADASTRO DE ENTIDADE</TituloSecao>
-				<FormularioEntidade enviarEntidade={enviarEntidade} />
-			</CadastroMain>
-		</>
-	);
+	useEffect(() => {
+		getToken();
+	}, []);
+
+	if (usuario)
+		return (
+			<>
+				<Menu />
+				<CadastroMain>
+					<TituloSecao>CADASTRO DE ENTIDADE</TituloSecao>
+					<FormularioEntidade enviarEntidade={enviarEntidade} />
+				</CadastroMain>
+			</>
+		);
+
+	return <></>;
 }
