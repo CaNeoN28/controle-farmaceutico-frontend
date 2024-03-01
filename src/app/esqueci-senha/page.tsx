1ae7fd02af6d130f6240529ea22dc32e6e50ddf7
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-03-01 16:05:25 -0300

    Iniciando esqueci-senha

---
+++ src/app/esqueci-senha/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Menu from "@/components/Menu";
+import styles from "./EsqueciSenha.module.scss";
+
+export default function EsqueciSenha() {
+	return (
+		<>
+			<Menu />
+			<form></form>
+		</>
+	);
+}
