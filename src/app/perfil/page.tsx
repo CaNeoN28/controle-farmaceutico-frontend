088a8cde39e3c2d38a37d26ccba6087bd4ea4576
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-03-01 09:58:05 -0300

    Pegando dados do usuário

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -3,14 +3,51 @@
 import Menu from "@/components/Menu";
 import styles from "./Perfil.module.scss";
 import { CadastroContainer } from "@/components/Cadastro";
+import { useEffect, useState } from "react";
+import IUsuarioGet, { IUsuarioAPI } from "@/types/Usuario";
+import FetchAutenticacao from "@/fetch/autenticacao";
+import { deleteCookie, getCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
 
 export default function Perfil() {
-	return (
-		<>
-			<Menu />
-			<main className={styles.main}>
-				<CadastroContainer></CadastroContainer>
-			</main>
-		</>
-	);
+	const router = useRouter();
+
+	const fAuth = new FetchAutenticacao();
+
+	const [usuario, setUsuario] = useState<IUsuarioGet>();
+	const [token, setToken] = useState<string>();
+
+	async function getUsuario() {
+		const token = getCookie("authentication");
+
+		await fAuth
+			.getPerfil(token)
+			.then((res) => {
+				const usuario = res.data as IUsuarioGet;
+				console.log(usuario);
+
+				setUsuario(usuario);
+				setToken(token);
+			})
+			.catch(() => {
+				deleteCookie("authentication");
+				router.push("/login");
+			});
+	}
+
+	useEffect(() => {
+		getUsuario();
+	}, []);
+
+	if (usuario)
+		return (
+			<>
+				<Menu />
+				<main className={styles.main}>
+					<CadastroContainer></CadastroContainer>
+				</main>
+			</>
+		);
+
+	return <></>;
 }
