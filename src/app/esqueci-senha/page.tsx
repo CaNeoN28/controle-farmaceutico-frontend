1433ec96e94cb88b61fa7410305749a74d1b86b6
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-03-01 16:16:21 -0300

    Organizando componente

--- src/app/esqueci-senha/page.tsx
+++ src/app/esqueci-senha/page.tsx
@@ -3,12 +3,24 @@
 import Menu from "@/components/Menu";
 import styles from "./EsqueciSenha.module.scss";
 import CenterBox from "@/components/CenterBox";
+import InputContainer from "@/components/InputContainer";
+import Input from "@/components/Input";
 
 export default function EsqueciSenha() {
 	return (
 		<>
 			<Menu />
-			<CenterBox titulo="ESQUECEU A SENHA"></CenterBox>
+			<CenterBox
+				titulo="ESQUECEU A SENHA"
+				cancelText="Cancelar"
+				onCancel={() => {}}
+				submitText="Enviar email"
+				onSubmit={() => {}}
+			>
+				<InputContainer id="email" label="Email">
+					<Input id="email" />
+				</InputContainer>
+			</CenterBox>
 		</>
 	);
 }
