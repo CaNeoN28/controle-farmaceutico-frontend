1bfb49d02eb49b4a511aa772f95f07533eb75a50
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-03-01 17:00:02 -0300

    Finalizando estilização

--- src/app/email-enviado/page.tsx
+++ src/app/email-enviado/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 
+import styles from "./EmailEnviado.module.scss";
 import CenterBox from "@/components/CenterBox";
 import Menu from "@/components/Menu";
+import { useRouter } from "next/navigation";
 
 export default function EmailEnviado() {
+	const router = useRouter();
+
 	return (
 		<>
 			<Menu />
-			<main>
-				<CenterBox >
-
+			<main className={styles.main}>
+				<CenterBox
+					titulo="EMAIL ENVIADO"
+					submitText="Confirmar"
+					onSubmit={() => {
+						router.push("/");
+					}}
+				>
+					<span className={styles.mensagem}>
+						Por favor verifique na sua caixa de email
+					</span>
 				</CenterBox>
 			</main>
 		</>
