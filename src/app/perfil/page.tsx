500b39b91d4188fece7e9a7cb8095f7867fd42ed
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-03-01 10:07:59 -0300

    Passando dados para um formulário

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -2,17 +2,42 @@
 
 import Menu from "@/components/Menu";
 import styles from "./Perfil.module.scss";
-import { CadastroContainer } from "@/components/Cadastro";
+import { CadastroContainer, CadastroForm } from "@/components/Cadastro";
 import { useEffect, useState } from "react";
-import IUsuarioGet, { IUsuarioAPI } from "@/types/Usuario";
+import IUsuarioGet, {
+	IUsuarioAPI,
+	IUsuarioCadastro,
+	IUsuarioPost,
+} from "@/types/Usuario";
 import FetchAutenticacao from "@/fetch/autenticacao";
 import { deleteCookie, getCookie } from "cookies-next";
 import { useRouter } from "next/navigation";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import InputContainer from "@/components/InputContainer";
+import Input from "@/components/Input";
 
 export default function Perfil() {
 	const router = useRouter();
 
 	const fAuth = new FetchAutenticacao();
+
+	const {
+		control,
+		formState: { errors },
+		watch,
+		setValue,
+		handleSubmit,
+	} = useForm<IUsuarioCadastro>({
+		defaultValues: {
+			nome_completo: "",
+			cpf: "",
+			numero_registro: "",
+			nome_usuario: "",
+			email: "",
+			senha: "",
+			confirmacao_senha: "",
+		},
+	});
 
 	const [usuario, setUsuario] = useState<IUsuarioGet>();
 	const [token, setToken] = useState<string>();
@@ -35,16 +60,46 @@ export default function Perfil() {
 			});
 	}
 
+	const onUpdate: SubmitHandler<IUsuarioCadastro> = async function (data) {
+		console.log(data);
+	};
+
 	useEffect(() => {
 		getUsuario();
 	}, []);
+
+	useEffect(() => {
+		if (usuario) {
+			setValue("nome_completo", usuario.nome_completo);
+			setValue("cpf", usuario.cpf);
+			setValue("numero_registro", usuario.numero_registro);
+			setValue("nome_usuario", usuario.nome_usuario);
+			setValue("email", usuario.email);
+		}
+	}, [usuario]);
 
 	if (usuario)
 		return (
 			<>
 				<Menu />
 				<main className={styles.main}>
-					<CadastroContainer></CadastroContainer>
+					<CadastroContainer>
+						<Controller
+							control={control}
+							name="nome_completo"
+							disabled
+							render={({ field }) => {
+								return (
+									<InputContainer
+										id="nome_completo"
+										label="Nome completo"
+									>
+										<Input id="nome_completo" {...{ ...field, ref: null }} />
+									</InputContainer>
+								);
+							}}
+						/>
+					</CadastroContainer>
 				</main>
 			</>
 		);
