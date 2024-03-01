b0d0441b820ca759d99590cc8eeff2fab7402881
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-03-01 16:39:53 -0300

    Usando hookForms

--- src/app/esqueci-senha/page.tsx
+++ src/app/esqueci-senha/page.tsx
@@ -5,8 +5,31 @@ import styles from "./EsqueciSenha.module.scss";
 import CenterBox from "@/components/CenterBox";
 import InputContainer from "@/components/InputContainer";
 import Input from "@/components/Input";
+import { useRouter } from "next/navigation";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import regexValidation from "@/utils/regexValidation";
 
 export default function EsqueciSenha() {
+	const router = useRouter();
+
+	const {
+		control,
+		formState: { errors },
+		handleSubmit,
+	} = useForm<{ email: string }>({
+		defaultValues: {
+			email: "",
+		},
+	});
+
+	const onCancel = () => {
+		router.back();
+	};
+
+	const onSubmit: SubmitHandler<{ email: string }> = (data) => {
+		console.log(data);
+	};
+
 	return (
 		<>
 			<Menu />
@@ -14,13 +37,31 @@ export default function EsqueciSenha() {
 				<CenterBox
 					titulo="ESQUECEU A SENHA"
 					cancelText="Cancelar"
-					onCancel={() => {}}
+					onCancel={onCancel}
 					submitText="Enviar email"
-					onSubmit={() => {}}
+					onSubmit={handleSubmit(onSubmit)}
 				>
-					<InputContainer id="email" label="Email">
-						<Input id="email" />
-					</InputContainer>
+					<Controller
+						control={control}
+						name="email"
+						rules={{
+							required: {
+								message: "Email é obrigatório",
+								value: true,
+							},
+							pattern: {
+								message: "Email inválido",
+								value: regexValidation.EMAIL,
+							},
+						}}
+						render={({ field }) => {
+							return (
+								<InputContainer id="email" label="Email" error={errors.email}>
+									<Input id="email" {...{ ...field, ref: null }} />
+								</InputContainer>
+							);
+						}}
+					/>
 				</CenterBox>
 			</main>
 		</>
