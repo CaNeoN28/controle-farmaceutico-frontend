f85d54b83a35089dfbcd08beeeb6a23e4830ac47
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2024-03-08 08:58:14 -0300

    Usando geocoding somente quando todo o endereço for digitado

--- src/utils/geocoder.ts
+++ src/utils/geocoder.ts
@@ -5,43 +5,59 @@ import { Loader } from "@googlemaps/js-api-loader";
 export async function EncontrarCoordenada(
 	endereco: Endereco & { nome_farmacia: string }
 ) {
-	const loader = new Loader({
-		apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY || "",
-	});
-
-	const { Geocoder } = (await google.maps.importLibrary(
-		"geocoding"
-	)) as google.maps.GeocodingLibrary;
-
-	const geocoder = new Geocoder();
-
 	const { cep, estado, municipio, bairro, logradouro, numero, nome_farmacia } =
 		endereco;
-	const pesquisa = [
-		cep,
-		estado,
-		municipio,
-		bairro,
-		logradouro,
-		numero,
-		nome_farmacia,
-	].join(" ");
 
-	let map_center: Coordenadas = { lat: 0, lng: 0 };
+	if (
+		cep &&
+		estado &&
+		municipio &&
+		bairro &&
+		logradouro &&
+		numero &&
+		nome_farmacia
+	) {
+		const loader = new Loader({
+			apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY || "",
+		});
 
-	await geocoder
-		.geocode({ address: pesquisa })
-		.then((res) => {
-			const resultado = res.results[0].geometry.location.toJSON();
+		const { Geocoder } = (await google.maps.importLibrary(
+			"geocoding"
+		)) as google.maps.GeocodingLibrary;
 
-			if (
-				resultado.lat != map_center?.lat &&
-				resultado.lng != map_center?.lng
-			) {
-				map_center = resultado;
-			}
-		})
-		.catch(() => {});
+		const geocoder = new Geocoder();
 
-	return map_center;
+		const pesquisa = [
+			cep,
+			estado,
+			municipio,
+			bairro,
+			logradouro,
+			numero,
+			nome_farmacia,
+		].join(" ");
+
+		let map_center: Coordenadas = { lat: 0, lng: 0 };
+
+		await geocoder
+			.geocode({ address: pesquisa })
+			.then((res) => {
+				const resultado = res.results[0].geometry.location.toJSON();
+
+				if (
+					resultado.lat != map_center?.lat &&
+					resultado.lng != map_center?.lng
+				) {
+					map_center = resultado;
+				}
+			})
+			.catch(() => {});
+
+		return map_center;
+	}
+
+	return {
+		lat: 0,
+		lng: 0,
+	};
 }
